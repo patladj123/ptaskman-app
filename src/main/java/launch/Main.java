02742d1bcd5fdd44ba24f4d84502616f411af251
patladj <F8034RhTie435grt345gt>
2017-06-21 01:30:29 +0300

    More - remove reqiremenent for Java 1.8 from code

--- src/main/java/launch/Main.java
+++ src/main/java/launch/Main.java
@@ -1,21 +1,22 @@
 /* Copyright 2015 Oracle and/or its affiliates. All rights reserved. */
 package launch;
 
-import java.util.Optional;
-
 import org.apache.catalina.startup.Tomcat;
 
 public class Main {
     
-    public static final Optional<String> PORT = Optional.ofNullable(System.getenv("PORT"));
-    public static final Optional<String> HOSTNAME = Optional.ofNullable(System.getenv("HOSTNAME"));
+    public static String PORT = System.getenv("PORT");
+    public static String HOSTNAME = System.getenv("HOSTNAME");
     
     public static void main(String[] args) throws Exception {
+        if (PORT==null || PORT.length()==0) PORT="8080";
+        if (HOSTNAME==null || HOSTNAME.length()==0) HOSTNAME="8080";
+
         String contextPath = "/" ;
         String appBase = ".";
         Tomcat tomcat = new Tomcat();   
-        tomcat.setPort(Integer.valueOf(PORT.orElse("8080") ));
-        tomcat.setHostname(HOSTNAME.orElse("localhost"));
+        tomcat.setPort(Integer.valueOf(PORT ));
+        tomcat.setHostname(HOSTNAME);
         tomcat.getHost().setAppBase(appBase);
         tomcat.addWebapp(contextPath, appBase);
 //        tomcat.addWebapp(host, url, name, path)
